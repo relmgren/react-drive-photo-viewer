73f434ec370e274a7f60f37021a3426f3551ce3d
Rasmus Elmgren <elmgren93@gmail.com>
2017-06-17 22:39:01 +0200

    added enzyme for testing and created a Error component

---
+++ src/routes/error/Error.js
@@ -0,0 +1,12 @@
+import React, {  Component } from 'react';
+import './error.css';
+
+class Error extends Component {
+  render = () => {
+    return (
+      <h2 className=".h2--text-color-red">Error rendering page</h2>
+    )
+  }
+}
+
+export default Error;
