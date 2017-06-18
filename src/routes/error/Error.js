dd796cfa1b6db908c1fd10bdd990c455e3b2e4a2
Rasmus Elmgren <elmgren93@gmail.com>
2017-06-18 10:00:09 +0200

    refactored error page to be a dumb component

--- src/routes/error/Error.js
+++ src/routes/error/Error.js
@@ -1,12 +1,10 @@
-import React, {  Component } from 'react';
+import React from 'react';
 import './error.css';
 
-class Error extends Component {
-  render = () => {
-    return (
-      <h2 className=".h2--text-color-red">Error rendering page</h2>
-    )
-  }
+const Error = () => {
+  return (
+    <h2 className=".h2--text-color-red">Error rendering page</h2>
+  )
 }
 
 export default Error;
